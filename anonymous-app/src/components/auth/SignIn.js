4c4d695622944e5a7a27b01438197c65c824f89c
ipek <ipekcivicakan@gmail.com>
2019-03-18 14:39:09 -0700

    Login & SignUp Components

---
+++ anonymous-app/src/components/auth/SignIn.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+
+class SignIn extends Component {
+
+    state = {
+        email: '',
+        password: ''
+    }
+    handleChange = (e) => {
+        this.setState({
+            [e.target.id]: e.target.value
+        })
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(this.state);
+    }
+
+    render() {
+
+        return (
+            <div className="signin-container">
+                <form onSubmit={this.handleSubmit}>
+                    <h5 className="sign-title">Sign In</h5>
+                    <div className="input-field">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" onChange={this.handleChange} />
+                    </div>
+                    <div className="input-field">
+                        <label htmlFor="password">Password</label>
+                        <input type="password" id="password" onChange={this.handleChange} />
+                    </div>
+                    <div className="input-field">
+                        <button className="btn-dark">Login</button>
+                    </div>
+                </form>
+
+            </div>
+        )
+    }
+}
+
+export default SignIn
