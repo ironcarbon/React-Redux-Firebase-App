0e012692280ce3ff29d07947dc789c0186b9396f
ipek <ipekcivicakan@gmail.com>
2019-03-22 17:52:59 -0700

    Cloud Functions Setup

--- firestoreSecurity.js
+++ firestoreSecurity.js
@@ -1,15 +1,15 @@
 //Firestore security rules
 
-service cloud.firestore {
-    match / databases / { database } / documents {
-        match / confessions / { confession } {
-            allow read, write: if request.auth.uid != null
-      }
-        match / users / { userId }{
-            allow create
-            allow read: if request.auth.uid != null
-        allow write: if request.auth.uid == userId
-      }
-    }
-}
+// service cloud.firestore {
+//     match / databases / { database } / documents {
+//         match / confessions / { confession } {
+//             allow read, write: if request.auth.uid != null
+//       }
+//         match / users / { userId }{
+//             allow create
+//             allow read: if request.auth.uid != null
+//         allow write: if request.auth.uid == userId
+//       }
+//     }
+// }
 
